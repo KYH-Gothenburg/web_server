76f87936a72883273ba318b63abb66c8e0bee999
Joakim Wassberg <joakim@arthead.se>
2020-04-21 12:03:58 +0200

    Initial commit

---
+++ server.py
@@ -0,0 +1,117 @@
+import socket
+
+
+class TCPServer:
+    host = '127.0.0.1'
+    port = 8888
+
+    def start(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind((self.host, self.port))
+
+        s.listen(5)
+        print("Listening at", s.getsockname())
+
+        while True:
+            conn, addr = s.accept()
+            print("Connected to", addr)
+            data = conn.recv(1024)
+
+            response = self.handle_request(data)
+            conn.sendall(bytes(response, 'utf-8'))
+            conn.close()
+
+    def handle_request(self, data):
+        """
+        Handles incoming requests. Must be overridden in subclass
+        :param data: request header
+        :return: response
+        """
+        return data
+
+
+class HTTPServer(TCPServer):
+    headers = {
+        'Server': 'The Ultimate Super Server',
+        'Content-Type': 'text/html',
+    }
+    status_codes = {
+        200: 'OK',
+        404: 'Not Found',
+        501: 'Not Implemented'
+    }
+
+    def handle_request(self, data):
+        request = HTTPRequest()
+        request.parse(data)
+        if request.method is not None:
+            try:
+                handler = getattr(self, f'handle_{request.method.lower()}')
+            except AttributeError:
+                handler = self.handle_501
+        else:
+            return
+        result = handler()
+        return result
+
+    def handle_get(self):
+        pass
+
+    def handle_options(self):
+        response_line = self.response_line(200)
+        extra_headers = {
+            'Allow': 'OPTIONS, GET'
+        }
+        response_headers = self.response_headers(extra_headers)
+        return f"{response_line}{response_headers}\r\n"
+
+    def handle_404(self):
+        pass
+
+    def handle_501(self):
+        response_line = self.response_line(501)
+        response_headers = self.response_headers()
+        response_body = "<h1>501 Not Implemented</h1>"
+        return f"{response_line}{response_headers}\r\n{response_body}"
+
+    def response_line(self, status_code):
+        reason = self.status_codes[status_code]
+        return f"HTTP/1.1 {status_code} {reason}\r\n"
+
+    def response_headers(self, extra_headers=None):
+        headers_copy = self.headers.copy()
+        if extra_headers:
+            headers_copy.update(extra_headers)
+        headers = ""
+        for h in headers_copy:
+            headers += f"{h}: {headers_copy[h]}\r\n"
+        return headers
+
+
+class HTTPRequest:
+    def __init__(self):
+        self.method = None
+        self.uri = None
+        self.http_version = 'HTTP/1.1'
+        self.headers = {}
+
+    def parse(self, data):
+        line = str(data).split("\\r\\n")
+        line = line[0].replace("b\'", "")
+        line = line.replace('b"', "")
+        lines = line.split(" ")
+        if len(lines) == 1:
+            return
+        self.method = lines[0]
+        self.uri = lines[1]
+        if len(lines) > 2:
+            self.http_version = lines[2]
+
+
+def main():
+    server = HTTPServer()
+    server.start()
+
+
+if __name__ == '__main__':
+    main()
